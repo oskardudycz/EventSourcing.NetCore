db8615b777444efcd61b73bd2efe9ac36cfabf79
Oskar Dudycz <o.dudycz@lgbs.pl>
2017-01-28 12:59:41 +0100

    XUnit Config

---
+++ Domain.Tests/TestClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Domain.Tests
+{
+    public class TestClass
+    {
+
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, 4);
+        }
+    }
+}
