ba08f8e64afff4f8e110fac65b886a1e8ff3bfe1
Oskar Dudycz <o.dudycz@lgbs.pl>
2017-02-06 19:42:13 +0100

    MediatR publishign tests

--- MediatR.Tests/Publishing/Publishing.cs
+++ MediatR.Tests/Publishing/Publishing.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
+using System.Threading.Tasks;
 
 namespace MediatR.Tests.Publishing
 {
@@ -12,7 +13,7 @@ namespace MediatR.Tests.Publishing
         {
             private readonly Dictionary<Type, List<object>> Services = new Dictionary<Type, List<object>>();
 
-            public void Register(Type type, params object [] implementations)
+            public void Register(Type type, params object[] implementations)
                 => Services.Add(type, implementations.ToList());
 
             public List<object> Get(Type type) { return Services[type]; }
@@ -90,6 +91,48 @@ namespace MediatR.Tests.Publishing
             //Registration needed internally by MediatR
             serviceLocator.Register(typeof(IPipelineBehavior<GetTaskNamesQuery, List<string>>), new object[] { });
             serviceLocator.Register(typeof(IRequestHandler<GetTaskNamesQuery, List<string>>), queryHandler);
+
+            var mediator = new Mediator(
+                    type => serviceLocator.Get(type).FirstOrDefault(),
+                    type => serviceLocator.Get(type));
+
+            var query = new GetTaskNamesQuery("cleaning");
+
+            //When
+            var result = await mediator.Send(query);
+
+            //Then
+            result.Should().Have.Count.EqualTo(2);
+            result.Should().Have.SameValuesAs("Cleaning main room", "cleaning kitchen");
+        }
+
+        public class GetTaskNamesQueryAsyncHandler : IAsyncRequestHandler<GetTaskNamesQuery, List<string>>
+        {
+            private readonly TasksList _taskList;
+            public GetTaskNamesQueryAsyncHandler(TasksList tasksList)
+            {
+                _taskList = tasksList;
+            }
+
+            public Task<List<string>> Handle(GetTaskNamesQuery query)
+            {
+                return Task.Run(() => _taskList.Tasks
+                    .Where(taskName => taskName.ToLower().Contains(query.Filter.ToLower()))
+                    .ToList());
+            }
+        }
+
+        [Fact]
+        public async void GivenRegisteredAsynchronousQueryHandler_WhenPublishMethodIsBeingCalled_ThenReturnsProperResult()
+        {
+            //Given
+            var queryHandler = new GetTaskNamesQueryAsyncHandler(
+                new TasksList("Cleaning main room", "Writing blog", "cleaning kitchen"));
+
+            var serviceLocator = new ServiceLocator();
+            //Registration needed internally by MediatR
+            serviceLocator.Register(typeof(IPipelineBehavior<GetTaskNamesQuery, List<string>>), new object[] { });
+            serviceLocator.Register(typeof(IAsyncRequestHandler<GetTaskNamesQuery, List<string>>), queryHandler);
 
             var mediator = new Mediator(
                     type => serviceLocator.Get(type).FirstOrDefault(),
