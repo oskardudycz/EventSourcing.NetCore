7c84315fcf774a415810effed4a7ff8db599c642
Oskar Dudycz <oskar.dudycz@ac-project.net>
2017-05-06 11:45:49 +0200

    #3 - Fixed serialization od AccountSummaryView

--- Sample/EventSourcing.Sample.Transactions/Views/Accounts/AccountSummary/AccountSummaryView.cs
+++ Sample/EventSourcing.Sample.Transactions/Views/Accounts/AccountSummary/AccountSummaryView.cs
@@ -6,13 +6,13 @@ namespace EventSourcing.Sample.Transactions.Views.Accounts.AccountSummary
 {
     public class AccountSummaryView
     {
-        public Guid Id { get; private set; }
+        public Guid Id { get; set; }
 
-        public Guid AccountId { get; private set; }
-        public Guid ClientId { get; private set; }
-        public string Number { get; private set; }
-        public decimal Balance { get; private set; }
-        public int TransactionsCount { get; private set; }
+        public Guid AccountId { get; set; }
+        public Guid ClientId { get; set; }
+        public string Number { get; set; }
+        public decimal Balance { get; set; }
+        public int TransactionsCount { get; set; }
 
         public void ApplyEvent(NewAccountCreated @event)
         {
