d598b94a7c39cfa5d3c5d4eb8af6d0d2f8e7411f
Oskar Dudycz <oskar.dudycz@gmail.com>
2018-06-16 09:33:04 +0200

    Downgraded version of EntityFramework, because it uses newer version of Npgsql because it collides with Marten

--- Sample/EventSourcing.Sample.Clients/Migrations/ClientsDbContextModelSnapshot.cs
+++ Sample/EventSourcing.Sample.Clients/Migrations/ClientsDbContextModelSnapshot.cs
@@ -6,7 +6,6 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore.Storage.Internal;
-using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using System;
 
 namespace EventSourcing.Sample.Clients.Migrations
