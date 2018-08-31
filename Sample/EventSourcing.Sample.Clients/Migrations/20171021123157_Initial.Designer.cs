ebe85109669c590935ed8b6ac9e68f12a3090cfb
Oskar Dudycz <oskar.dudycz@gmail.com>
2018-08-31 18:45:09 +0200

    Uupgrade to net core 2.1 (#13)
    Upgrade to net core 2.1

--- Sample/EventSourcing.Sample.Clients/Migrations/20171021123157_Initial.Designer.cs
+++ Sample/EventSourcing.Sample.Clients/Migrations/20171021123157_Initial.Designer.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.EntityFrameworkCore.Storage.Internal;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using System;
 
 namespace EventSourcing.Sample.Clients.Migrations
