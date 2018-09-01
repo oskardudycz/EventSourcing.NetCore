ff9d6b47213f407fbbc8a068495849f6dd8d39aa
Oskar Dudycz <oskar.dudycz@gmail.com>
2018-09-01 13:12:40 +0200

    Added sample integration test appsettings.json

--- Sample/EventSourcing.Sample.IntegrationTests/Infrastructure/TestContext.cs
+++ Sample/EventSourcing.Sample.IntegrationTests/Infrastructure/TestContext.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Net.Http;
 using EventSourcing.Web.Sample;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
 
 namespace EventSourcing.Sample.IntegrationTests.Infrastructure
 {
@@ -18,7 +20,16 @@ namespace EventSourcing.Sample.IntegrationTests.Infrastructure
 
         private void SetUpClient()
         {
+            var projectDir = Directory.GetCurrentDirectory();
+
             _server = new TestServer(new WebHostBuilder()
+                .UseEnvironment("Tests")
+                .UseContentRoot(projectDir)
+                .UseConfiguration(new ConfigurationBuilder()
+                    .SetBasePath(projectDir)
+                    .AddJsonFile("appsettings.json")
+                    .Build()
+                )
                 .UseStartup<Startup>());
 
             Client = _server.CreateClient();
