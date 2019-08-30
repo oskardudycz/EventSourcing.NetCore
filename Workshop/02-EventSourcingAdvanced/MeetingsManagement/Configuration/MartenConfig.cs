a212b339503c9427bb5cb842d892dbba0db51908
Oskar Dudycz <oskar.dudycz@gmail.com>
2019-08-30 16:22:33 +0200

    Added MartenConfig to be strongly typed class

--- Workshop/02-EventSourcingAdvanced/MeetingsManagement/Configuration/MartenConfig.cs
+++ Workshop/02-EventSourcingAdvanced/MeetingsManagement/Configuration/MartenConfig.cs
@@ -6,26 +6,32 @@ namespace MeetingsManagement.Configuration
 {
     public class MartenConfig
     {
+        public const string DefaultSchema = "public";
+
+        public string ConnectionString { get; set; }
+
+        public string WriteModelSchema { get; set; } = DefaultSchema;
+        public string ReadModelSchema { get; set; } = DefaultSchema;
     }
 
     internal static class MartenConfigExtensions
     {
+        public const string DefaultConfigKey = "EventStore";
+
         internal static void AddMarten(this IServiceCollection services, IConfiguration config)
         {
             services.AddSingleton<IDocumentStore>(sp => DocumentStore.For(options => SetStoreOptions(options, config)));
             services.AddSingleton(sp => sp.GetRequiredService<IDocumentStore>().OpenSession());
         }
 
-        private static void SetStoreOptions(StoreOptions options, IConfiguration config)
+        private static void SetStoreOptions(StoreOptions options, IConfiguration configuration)
         {
-            var martenConfig = config.GetSection("EventStore");
-            var connectionString = martenConfig.GetSection("ConnectionString").Value;
-            var schemaName = martenConfig.GetSection("Schema").Value;
+            var config = configuration.GetSection(DefaultConfigKey).Get<MartenConfig>();
 
-            options.Connection(connectionString);
+            options.Connection(config.ConnectionString);
             options.AutoCreateSchemaObjects = AutoCreate.All;
-            options.Events.DatabaseSchemaName = schemaName;
-            options.DatabaseSchemaName = schemaName;
+            options.Events.DatabaseSchemaName = config.WriteModelSchema;
+            options.DatabaseSchemaName = config.ReadModelSchema;
         }
     }
 }
