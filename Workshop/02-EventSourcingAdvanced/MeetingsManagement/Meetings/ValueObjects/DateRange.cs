889089fee115f03b60b2c8717b63d90b88417734
Oskar Dudycz <oskar.dudycz@gmail.com>
2020-01-07 20:35:03 +0100

    Updated AppVeyor config to Visual Studio 2019 Added missing DateRange Added VSCode tasks.json

---
+++ Workshop/02-EventSourcingAdvanced/MeetingsManagement/Meetings/ValueObjects/DateRange.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace MeetingsManagement.Meetings.ValueObjects
+{
+    public class DateRange
+    {
+        public DateTime Start { get; }
+        public DateTime End { get; }
+
+        public DateRange(DateTime start, DateTime end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public static DateRange Create(DateTime start, DateTime end)
+        {
+            if (start == default(DateTime))
+                throw new ArgumentException($"{nameof(start)} needs to be defined.");
+
+            if (end == default(DateTime))
+                throw new ArgumentException($"{nameof(end)} needs to be defined.");
+
+            if (start > end)
+                throw new ArgumentException($"{nameof(start)} needs to be earlier or equal {nameof(end)}.");
+
+            return new DateRange(start, end);
+        }
+    }
+}
