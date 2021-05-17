9c348dccb4c017c51a7660b6ce49780b8dfa4239
oskar.dudycz <oskar.dudycz@gmail.com>
2021-05-17 21:33:01 +0200

    Added example of basic CQRS using Endpoints, Nullable Reference Types, Records and other C# 8-9 goodies Added tests for Registering the Product Added tests for the Warehouse example's queries Updated project and test configuration to run migrations automatically

---
+++ Sample/Warehouse/Warehouse/Products/RegisteringProduct/RegisterProduct.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Warehouse.Core.Commands;
+using Warehouse.Products.Primitives;
+
+namespace Warehouse.Products.RegisteringProduct
+{
+    internal class HandleRegisterProduct : ICommandHandler<RegisterProduct>
+    {
+        private readonly Func<Product, CancellationToken, ValueTask> addProduct;
+        private readonly Func<SKU, CancellationToken, ValueTask<bool>> productWithSKUExists;
+
+        public HandleRegisterProduct(
+            Func<Product, CancellationToken, ValueTask> addProduct,
+            Func<SKU, CancellationToken, ValueTask<bool>> productWithSKUExists
+        )
+        {
+            this.addProduct = addProduct;
+            this.productWithSKUExists = productWithSKUExists;
+        }
+
+        public async ValueTask Handle(RegisterProduct command, CancellationToken ct)
+        {
+            var product = new Product(
+                command.ProductId,
+                command.SKU,
+                command.Name,
+                command.Description
+            );
+
+            if (await productWithSKUExists(command.SKU, ct))
+                throw new InvalidOperationException(
+                    $"Product with SKU `{command.SKU} already exists.");
+
+            await addProduct(product, ct);
+        }
+    }
+
+    public record RegisterProduct
+    {
+        public Guid ProductId { get;}
+
+        public SKU SKU { get; }
+
+        public string Name { get; }
+
+        public string? Description { get; }
+
+        private RegisterProduct(Guid productId, SKU sku, string name, string? description)
+        {
+            ProductId = productId;
+            SKU = sku;
+            Name = name;
+            Description = description;
+        }
+
+        public static RegisterProduct Create(Guid? id, string? sku, string? name, string? description)
+        {
+            if (!id.HasValue) throw new ArgumentNullException(nameof(id));
+            if (name == null) throw new ArgumentNullException(nameof(name));
+
+            return new RegisterProduct(id.Value, SKU.Create(sku), name, description);
+        }
+    }
+}
