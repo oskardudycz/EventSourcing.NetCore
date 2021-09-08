285c71885de2a1f47d747570a3ba9f5da6f1d3bd
Oskar Dudycz <oskar.dudycz@gmail.com>
2021-09-08 09:26:53 +0200

    Removed some files

--- Sample/Warehouse/Warehouse.Api/Program.cs
+++ Sample/Warehouse/Warehouse.Api/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Net;
+using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 using Core.WebApi.Middlewares.ExceptionHandling;
