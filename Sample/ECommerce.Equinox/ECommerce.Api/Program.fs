fc6b79bbdc3f72457d6733480d90cd77e4c1ccd1
Ruben Bartelink <ruben@bartelink.com>
2021-11-21 15:00:32 +0000

    Add API

---
+++ Sample/ECommerce.Equinox/ECommerce.Api/Program.fs
@@ -0,0 +1,36 @@
+namespace ECommerce.Api
+#nowarn "20"
+open System
+open System.Collections.Generic
+open System.IO
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.HttpsPolicy
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+
+module Program =
+    let exitCode = 0
+
+    [<EntryPoint>]
+    let main args =
+
+        let builder = WebApplication.CreateBuilder(args)
+
+        builder.Services.AddControllers()
+
+        let app = builder.Build()
+
+        app.UseHttpsRedirection()
+
+        app.UseAuthorization()
+        app.MapControllers()
+
+        app.Run()
+
+        exitCode
