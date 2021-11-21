20c6bfdee73915e1c74b1e41cac2987032eb2a2f
Ruben Bartelink <ruben@bartelink.com>
2021-11-21 08:34:50 +0000

    WIP

---
+++ Sample/ECommerce.Equinox/ECommerce.Domain/Types.fs
@@ -0,0 +1,28 @@
+namespace ECommerce.Domain
+
+open FSharp.UMX
+open System
+
+type ProductId = Guid<productId>
+and [<Measure>] productId
+module ProductId =
+    let toString (x : ProductId) : string = (UMX.untag x).ToString("N")
+    let parse (value : Guid) : ProductId = %value
+
+type ClientId = Guid<clientId>
+and [<Measure>] clientId
+module ClientId =
+    let toString (x : ClientId) : string = (UMX.untag x).ToString("N")
+    let parse (value : Guid Nullable) : ClientId =
+        if not value.HasValue || value.Value = Guid.Empty then raise <| ArgumentOutOfRangeException(nameof value)
+        %value.Value
+    let (|Parse|) = parse
+
+type CartId = Guid<cartId>
+and [<Measure>] cartId
+module CartId =
+    let toString (x : CartId) : string = (UMX.untag x).ToString("N")
+    let parse (value : Guid Nullable) : CartId =
+        if not value.HasValue || value.Value = Guid.Empty then raise <| ArgumentOutOfRangeException(nameof value)
+        %value.Value
+    let (|Parse|) = parse
