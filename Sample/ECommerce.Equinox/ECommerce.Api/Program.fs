e1b784aa84093acd810bc379134559b75287d720
Ruben Bartelink <ruben@bartelink.com>
2021-11-21 16:20:34 +0000

    Add Cosmos wiring for API startup

--- Sample/ECommerce.Equinox/ECommerce.Api/Program.fs
+++ Sample/ECommerce.Equinox/ECommerce.Api/Program.fs
@@ -1,36 +1,113 @@
-namespace ECommerce.Api
-#nowarn "20"
-open System
-open System.Collections.Generic
-open System.IO
-open System.Linq
-open System.Threading.Tasks
-open Microsoft.AspNetCore
-open Microsoft.AspNetCore.Builder
+module ECommerce.Api.Program
+
+open ECommerce
 open Microsoft.AspNetCore.Hosting
-open Microsoft.AspNetCore.HttpsPolicy
-open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
-open Microsoft.Extensions.Hosting
-open Microsoft.Extensions.Logging
+open Serilog
+open System
 
-module Program =
-    let exitCode = 0
+exception MissingArg of message : string with override this.Message = this.message
 
-    [<EntryPoint>]
-    let main args =
+type Configuration(tryGet) =
 
-        let builder = WebApplication.CreateBuilder(args)
+    let get key =
+        match tryGet key with
+        | Some value -> value
+        | None -> raise (MissingArg $"Missing Argument/Environment Variable %s{key}")
 
-        builder.Services.AddControllers()
+    member _.CosmosConnection =             get "EQUINOX_COSMOS_CONNECTION"
+    member _.CosmosDatabase =               get "EQUINOX_COSMOS_DATABASE"
+    member _.CosmosContainer =              get "EQUINOX_COSMOS_CONTAINER"
 
-        let app = builder.Build()
+module Args =
 
-        app.UseHttpsRedirection()
+    open Argu
+    [<NoEquality; NoComparison>]
+    type Parameters =
+        | [<AltCommandLine "-V"; Unique>]   Verbose
+        | [<CliPrefix(CliPrefix.None); Last>] Cosmos of ParseResults<CosmosParameters>
+        interface IArgParserTemplate with
+            member a.Usage = a |> function
+                | Verbose _ ->              "request verbose logging."
+                | Cosmos _ ->               "specify CosmosDb input parameters"
+    and Arguments(c : Configuration, a : ParseResults<Parameters>) =
+        member val Cosmos =                 CosmosArguments(c, a.GetResult Cosmos)
+        member val Verbose =                a.Contains Parameters.Verbose
+    and [<NoEquality; NoComparison>] CosmosParameters =
+        | [<AltCommandLine "-V"; Unique>]   Verbose
+        | [<AltCommandLine "-m">]           ConnectionMode of Microsoft.Azure.Cosmos.ConnectionMode
+        | [<AltCommandLine "-s">]           Connection of string
+        | [<AltCommandLine "-d">]           Database of string
+        | [<AltCommandLine "-c">]           Container of string
+        | [<AltCommandLine "-o">]           Timeout of float
+        | [<AltCommandLine "-r">]           Retries of int
+        | [<AltCommandLine "-rt">]          RetriesWaitTime of float
+        interface IArgParserTemplate with
+            member a.Usage = a |> function
+                | Verbose ->                "request Verbose Logging from Store. Default: off"
+                | ConnectionMode _ ->       "override the connection mode. Default: Direct."
+                | Connection _ ->           "specify a connection string for a Cosmos account. (optional if environment variable EQUINOX_COSMOS_CONNECTION specified)"
+                | Database _ ->             "specify a database name for Cosmos store. (optional if environment variable EQUINOX_COSMOS_DATABASE specified)"
+                | Container _ ->            "specify a container name for Cosmos store. (optional if environment variable EQUINOX_COSMOS_CONTAINER specified)"
+                | Timeout _ ->              "specify operation timeout in seconds (default: 5)."
+                | Retries _ ->              "specify operation retries (default: 1)."
+                | RetriesWaitTime _ ->      "specify max wait-time for retry when being throttled by Cosmos in seconds (default: 5)"
+    and CosmosArguments(c : Configuration, a : ParseResults<CosmosParameters>) =
+        let discovery =                     a.TryGetResult Connection |> Option.defaultWith (fun () -> c.CosmosConnection) |> Equinox.CosmosStore.Discovery.ConnectionString
+        let mode =                          a.TryGetResult ConnectionMode
+        let timeout =                       a.GetResult(Timeout, 5.) |> TimeSpan.FromSeconds
+        let retries =                       a.GetResult(Retries, 1)
+        let maxRetryWaitTime =              a.GetResult(RetriesWaitTime, 5.) |> TimeSpan.FromSeconds
+        let connector =                     Equinox.CosmosStore.CosmosStoreConnector(discovery, timeout, retries, maxRetryWaitTime, ?mode = mode)
+        let database =                      a.TryGetResult Database |> Option.defaultWith (fun () -> c.CosmosDatabase)
+        let container =                     a.TryGetResult Container |> Option.defaultWith (fun () -> c.CosmosContainer)
+        member _.Verbose =                  a.Contains Verbose
+        member _.Connect() =                connector.ConnectStore("Main", database, container)
 
-        app.UseAuthorization()
-        app.MapControllers()
+    /// Parse the commandline; can throw exceptions in response to missing arguments and/or `-h`/`--help` args
+    let parse tryGetConfigValue argv =
+        let programName = Reflection.Assembly.GetEntryAssembly().GetName().Name
+        let parser = ArgumentParser.Create<Parameters>(programName=programName)
+        Arguments(Configuration tryGetConfigValue, parser.ParseCommandLine argv)
 
-        app.Run()
+[<System.Runtime.CompilerServices.Extension>]
+type Logging() =
 
-        exitCode
+    [<System.Runtime.CompilerServices.Extension>]
+    static member Configure(c : LoggerConfiguration, appName) =
+        let customTags = ["app",appName]
+        c
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft.AspNetCore", Serilog.Events.LogEventLevel.Warning)
+            .WriteTo.Sink(Equinox.CosmosStore.Prometheus.LogSink(customTags))
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+
+let [<Literal>] AppName = "ECommerce.Web"
+
+let run (args : Args.Arguments) =
+    let store =
+        let context = args.Cosmos.Connect() |> Async.RunSynchronously |> CosmosStoreContext.create
+        let cache = Equinox.Cache (AppName, sizeMb = 10)
+        Domain.Config.Store.Cosmos (context, cache)
+    let carts = Domain.ShoppingCart.Config.create store
+    let registerServices (services: IServiceCollection) =
+        services.AddSingleton(carts) |> ignore
+    WebHostBuilder()
+        .UseKestrel()
+        .UseSerilog()
+        .ConfigureServices(registerServices)
+        .UseStartup<Startup>()
+        .Build()
+        .Run()
+
+[<EntryPoint>]
+let main argv =
+    try let args = Args.parse EnvVar.tryGet argv
+        try Log.Logger <- LoggerConfiguration().Configure(args.Verbose).Sinks(Sinks.tags AppName |> Sinks.equinoxMetricsOnly, args.Cosmos.Verbose).CreateLogger()
+            try run args; 0
+            with e when not (e :? MissingArg) -> Log.Fatal(e, "Exiting"); 2
+        finally Log.CloseAndFlush()
+    with MissingArg msg -> eprintfn $"%s{msg}"; 1
+        | :? Argu.ArguParseException as e -> eprintfn $"%s{e.Message}"; 1
+        | e -> eprintfn $"Exception %s{e.Message}"; 1
