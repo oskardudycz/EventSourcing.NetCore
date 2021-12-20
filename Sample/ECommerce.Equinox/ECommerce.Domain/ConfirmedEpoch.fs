702c29cf6c5708c8c41842451ea2f76788488ecd
Ruben Bartelink <ruben@bartelink.com>
2021-12-20 12:06:02 +0000

    ConfirmedIngester initial impl

---
+++ Sample/ECommerce.Equinox/ECommerce.Domain/ConfirmedEpoch.fs
@@ -0,0 +1,133 @@
+/// Tracks all Confirmed Carts in the system
+/// - Used to walk back through the history of all Carts in the system in approximate order of their processing
+/// - Limited to a certain reasonable count of items; snapshot of Items in an epoch needs to stay a sensible size
+/// The ConfirmedSeries holds a pointer to the current active epoch for each series
+/// Each successive epoch is identified by an index, i.e. ConfirmedEpoch-0_0, then ConfirmedEpoch-0_1
+module ECommerce.Domain.ConfirmedEpoch
+
+let [<Literal>] Category = "ConfirmedEpoch"
+let streamName epochId = FsCodec.StreamName.compose Category [ConfirmedSeriesId.toString ConfirmedSeriesId.wellKnownId; ConfirmedEpochId.toString epochId]
+
+// NB - these types and the union case names reflect the actual storage formats and hence need to be versioned with care
+[<RequireQualifiedAccess>]
+module Events =
+
+    type Ingested = { carts : Cart[] }
+     and Cart = { cartId : CartId; items : Item[] }
+     and Item = { productId : ProductId; unitPrice : decimal; quantity : int }
+    type Event =
+        | Ingested of Ingested
+        | Closed
+        interface TypeShape.UnionContract.IUnionContract
+    let codec = FsCodec.NewtonsoftJson.Codec.Create<Event>()
+
+let itemId (x : Events.Cart) : CartId = x.cartId
+let (|ItemIds|) : Events.Cart[] -> CartId[] = Array.map itemId
+
+module Fold =
+
+    type State = CartId[] * bool
+    let initial = [||], false
+    let evolve (ids, closed) = function
+        | Events.Ingested { carts = ItemIds ingestedIds } -> (Array.append ids ingestedIds, closed)
+        | Events.Closed                                   -> (ids, true)
+
+    let fold : State -> Events.Event seq -> State = Seq.fold evolve
+
+type Result = { accepted : CartId[]; closed : bool; residual : Events.Cart[] }
+
+let notAlreadyIn (ids : CartId seq) =
+    let ids = System.Collections.Generic.HashSet ids
+    fun (x : Events.Cart) -> (not << ids.Contains) x.cartId
+
+/// Manages ingestion of only items not already in the list
+/// Yields residual net of items already present in this epoch
+// NOTE See feedSource template for more advanced version handling splitting large input requests where epoch limit is strict
+let decide shouldClose candidates (currentIds, closed as state) =
+    match closed, candidates |> Array.filter (notAlreadyIn currentIds) with
+    | false, fresh ->
+        let added, events =
+            match fresh with
+            | [||] -> [||], []
+            | ItemIds freshIds ->
+                let closing = shouldClose currentIds freshIds
+                let ingestEvent = Events.Ingested { carts = fresh }
+                freshIds, if closing then [ ingestEvent ; Events.Closed ] else [ ingestEvent ]
+        let _, closed = Fold.fold state events
+        { accepted = added; closed = closed; residual = [||] }, events
+    | true, fresh ->
+        { accepted = [||]; closed = true; residual = fresh }, []
+
+// NOTE see feedSource for example of separating Service logic into Ingestion and Read Services in order to vary the folding and/or state held
+type Service internal
+    (   shouldClose : CartId[] -> CartId[] -> bool, // let outer layers decide whether ingestion should trigger closing of the batch
+        resolve_ : Equinox.ResolveOption option -> ConfirmedEpochId -> Equinox.Decider<Events.Event, Fold.State>) =
+    let resolve = resolve_ None
+    let resolveStale = resolve_ (Some Equinox.AllowStale)
+
+    /// Ingest the supplied items. Yields relevant elements of the post-state to enable generation of stats
+    /// and facilitate deduplication of incoming items in order to avoid null store round-trips where possible
+    member _.Ingest(epochId, items) : Async<Result> =
+        let decider = resolveStale epochId
+        /// NOTE decider which will initially transact against potentially stale cached state, which will trigger a
+        /// resync if another writer has gotten in before us. This is a conscious decision in this instance; the bulk
+        /// of writes are presumed to be coming from within this same process
+        decider.Transact(decide shouldClose items)
+
+    /// Returns all the items currently held in the stream (Not using AllowStale on the assumption this needs to see updates from other apps)
+    member _.Read epochId : Async<Fold.State> =
+        let decider = resolve epochId
+        decider.Query id
+
+module Config =
+
+    let private create_ shouldClose resolve = Service(shouldClose, resolve)
+    let private resolveStream opt = function
+        | Config.Store.Memory store ->
+            let cat = Config.Memory.create Events.codec Fold.initial Fold.fold store
+            fun sn -> cat.Resolve(sn, ?option = opt)
+        | Config.Store.Esdb (context, cache) ->
+            let cat = Config.Esdb.createUnoptimized Events.codec Fold.initial Fold.fold (context, cache)
+            fun sn -> cat.Resolve(sn, ?option = opt)
+        | Config.Store.Cosmos (context, cache) ->
+            let cat = Config.Cosmos.createUnoptimized Events.codec Fold.initial Fold.fold (context, cache)
+            fun sn -> cat.Resolve(sn, ?option = opt)
+    let private resolveDecider store opt = streamName >> resolveStream opt store >> Config.createDecider
+    let create shouldClose = resolveDecider >> create_ shouldClose
+
+/// Custom Fold and caching logic compared to the IngesterService
+/// - When reading, we want the full Items
+/// - Caching only for one minute
+/// - There's no value in using the snapshot as it does not have the full state
+module Reader =
+
+    type State = Events.Cart[] * bool
+    let initial = [||], false
+    let evolve (es, closed) = function
+        | Events.Ingested e    -> Array.append es e.carts, closed
+        | Events.Closed        -> (es, true)
+    let fold : State -> Events.Event seq -> State = Seq.fold evolve
+
+    type StateDto = { closed : bool; carts : Events.Cart[] }
+
+    type Service internal (resolve : ConfirmedEpochId -> Equinox.Decider<Events.Event, State>) =
+
+        /// Returns all the items currently held in the stream
+        member _.Read(epochId) : Async<StateDto> =
+            let decider = resolve epochId
+            decider.Query(fun (carts, closed) -> { closed = closed; carts = carts })
+
+    module Config =
+
+        let private resolveStream = function
+            | Config.Store.Memory store ->
+                let cat = Config.Memory.create Events.codec initial fold store
+                cat.Resolve
+            | Config.Store.Esdb (context, cache) ->
+                let cat = Config.Esdb.createUnoptimized Events.codec initial fold (context, cache)
+                cat.Resolve
+            | Config.Store.Cosmos (context, cache) ->
+                let cat = Config.Cosmos.createUnoptimized Events.codec initial fold (context, cache)
+                cat.Resolve
+        let private resolveDecider store = streamName >> resolveStream store >> Config.createDecider
+        let create = resolveDecider >> Service
--- Sample/ECommerce.Equinox/ECommerce.Domain/Types.fs
+++ Sample/ECommerce.Equinox/ECommerce.Domain/Types.fs
@@ -3,6 +3,10 @@ namespace ECommerce.Domain
 open FSharp.UMX
 open System
 
+module Guid =
+
+    let inline toStringN (x : Guid) = x.ToString "N"
+
 type ProductId = Guid<productId>
 and [<Measure>] productId
 module ProductId =
@@ -28,3 +32,21 @@ module CartId =
         %value.Value
     let (|Parse|) = parse
     let generate () : CartId = Guid.NewGuid() |> Nullable |> parse
+
+(* At present, there's only a single series of Confirmed carts; this extension point could be used
+   e.g. if one was to implement multi-tenancy, the tenantId would become the seriesId *)
+
+type [<Measure>] confirmedSeriesId
+type ConfirmedSeriesId = int<confirmedSeriesId>
+module ConfirmedSeriesId =
+    let wellKnownId = 0<confirmedSeriesId>
+    let toString (value : ConfirmedSeriesId) : string = string %value
+
+type [<Measure>] confirmedEpochId
+type ConfirmedEpochId = int<confirmedEpochId>
+module ConfirmedEpochId =
+    let unknown = -1<confirmedEpochId>
+    let initial = 0<confirmedEpochId>
+    let value (value : ConfirmedEpochId) : int = %value
+    let next (value : ConfirmedEpochId) : ConfirmedEpochId = % (%value + 1)
+    let toString (value : ConfirmedEpochId) : string = string %value
