b974370592c6fa320b07302047483c5f91a93a15
Oskar Dudycz <oskar.dudycz@gmail.com>
2022-01-19 20:45:44 +0100

    Added examples of retry scope and Gap Measurement in ESDB

---
+++ Core.EventStoreDB/GapMeasurement/GapMeasurement.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using EventStore.Client;
+
+namespace Core.EventStoreDB.GapMeasurement;
+
+public static class GapMeasurement
+{
+    public static async Task<ulong> GetStreamSubscriptionGap
+    (
+        this EventStoreClient eventStore,
+        string streamName,
+        StreamPosition subscriptionLastPosition,
+        CancellationToken cancellationToken
+    )
+    {
+        var getCurrentLastEvent = await eventStore
+            .ReadStreamAsync(
+                Direction.Backwards,
+                streamName,
+                StreamPosition.End,
+                1,
+                resolveLinkTos: false,
+                cancellationToken: cancellationToken
+            ).LastAsync(cancellationToken);
+
+        var currentLastPosition = getCurrentLastEvent.Event.EventNumber.ToUInt64();
+
+        return subscriptionLastPosition - currentLastPosition;
+    }
+
+    public static async Task<ulong> GetAllStreamSubscriptionGap
+    (
+        this EventStoreClient eventStore,
+        Position subscriptionLastPosition,
+        CancellationToken cancellationToken
+    )
+    {
+        var getCurrentLastEvent = await eventStore
+            .ReadAllAsync(
+                Direction.Backwards,
+                Position.End,
+                1,
+                resolveLinkTos: false,
+                cancellationToken: cancellationToken
+            ).LastAsync(cancellationToken);
+
+        var currentLastPosition = getCurrentLastEvent.Event.Position.CommitPosition;
+
+        return subscriptionLastPosition.CommitPosition - currentLastPosition;
+    }
+
+    private const uint SubscriptionThreshold = 10;
+
+    public static async Task<bool> IsLive(Func<Task<ulong>> getSubscriptionGap)
+    {
+        var subscriptionGap = await getSubscriptionGap();
+
+        return subscriptionGap < SubscriptionThreshold;
+    }
+}
---
+++ Core.EventStoreDB/Retries/RetryScope.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using EventStore.Client;
+using Polly;
+using Polly.Wrap;
+
+namespace Core.EventStoreDB.Retries;
+
+public static class RetryScope
+{
+    private static readonly AsyncPolicyWrap RetryPolicy
+        = Policy
+            .Handle<Exception>()
+            .RetryAsync(7)
+            .WrapAsync(Policy.TimeoutAsync(5));
+
+    public static Task ExecuteAsync(
+        this EventStoreClient eventStore,
+        Func<EventStoreClient, CancellationToken, Task> action,
+        CancellationToken cancellationToken
+    ) =>
+        RetryPolicy.ExecuteAsync(ct => action(eventStore, ct), cancellationToken);
+
+    public static Task<TResult> ExecuteAsync<TResult>(
+        this EventStoreClient eventStore,
+        Func<EventStoreClient, CancellationToken, Task<TResult>> action,
+        CancellationToken cancellationToken
+    ) =>
+        RetryPolicy.ExecuteAsync(ct => action(eventStore, ct), cancellationToken);
+
+    public static Task<IWriteResult> AppendWithRetry(
+        this EventStoreClient eventStore,
+        string streamName,
+        StreamRevision expectedRevision,
+        IEnumerable<EventData> eventData,
+        CancellationToken cancellationToken
+    ) =>
+        eventStore.ExecuteAsync(
+            (es, ct) => es.AppendToStreamAsync(streamName, expectedRevision, eventData, cancellationToken: ct),
+            cancellationToken);
+}
