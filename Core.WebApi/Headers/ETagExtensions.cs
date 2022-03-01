535bb985bfea27bae57426175d397aef96720e6d
Oskar Dudycz <oskar.dudycz@gmail.com>
2022-03-01 09:18:43 +0100

    Moved ResourceVersion providers into Core project. Finished OptimisticConcurrencyMiddleware implementation.

---
+++ Core.WebApi/Headers/ETagExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Core.WebApi.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+
+namespace Core.WebApi.Headers;
+
+public static class ETagExtensions
+{
+    public static EntityTagHeaderValue? GetIfMatchRequestHeader(this HttpContext context) =>
+        context.Request.GetTypedHeaders().IfMatch.FirstOrDefault();
+
+    public static void TrySetETagResponseHeader(HttpContext context, string etag)
+    {
+        if (!context.IsSuccessful()) return;
+
+        context.Response.GetTypedHeaders().ETag = new EntityTagHeaderValue(etag, true);
+    }
+}
