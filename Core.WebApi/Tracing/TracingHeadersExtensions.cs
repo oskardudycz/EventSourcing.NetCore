2c07214494cf4644d518753bf801e500f341fbfa
Oskar Dudycz <oskar.dudycz@gmail.com>
2022-03-05 17:07:10 +0100

    Added missing logic in TracingMiddleware to actually get correlation and causation ids from request headers

---
+++ Core.WebApi/Tracing/TracingHeadersExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Core.Tracing.Causation;
+using Core.Tracing.Correlation;
+using Microsoft.AspNetCore.Http;
+
+namespace Core.WebApi.Tracing;
+
+public static class TracingHeadersExtensions
+{
+    private const string CorrelationIdHeaderName = "X-Correlation-ID";
+    private const string CausationIdHeaderName = "X-Causation-ID";
+
+    public static CorrelationId? GetCorrelationId(this HttpRequest request) =>
+        request.Headers.TryGetValue(CorrelationIdHeaderName, out var correlationId)
+            ? new CorrelationId(correlationId)
+            : null;
+
+
+    public static CausationId? GetCausationId(this HttpRequest request) =>
+        request.Headers.TryGetValue(CausationIdHeaderName, out var correlationId)
+            ? new CausationId(correlationId)
+            : null;
+
+    public static void SetCorrelationId(this HttpResponse response, CorrelationId correlationId) =>
+        response.Headers.Add(CorrelationIdHeaderName, new[] { correlationId.Value });
+
+
+    public static void SetCausationId(this HttpResponse response, CausationId causationId) =>
+        response.Headers.Add(CausationIdHeaderName, new[] { causationId.Value });
+
+
+}
