07e14b914a77dd14d39744eb8f5e7794d0a64837
grzegorzorwat <grzegorzorwat+github@protonmail.com>
2023-01-27 19:16:50 +0100

    Added product items validation to cart confirmation
    Order for confirmed cart can't be created when the cart is empty.

--- Sample/ECommerce/Carts/Carts.Tests/Builders/CartBuilder.cs
+++ Sample/ECommerce/Carts/Carts.Tests/Builders/CartBuilder.cs
@@ -1,11 +1,16 @@
+using Carts.Pricing;
 using Carts.ShoppingCarts;
+using Carts.ShoppingCarts.Products;
+using Carts.Tests.Stubs.Products;
 using Core.Aggregates;
 
 namespace Carts.Tests.Builders;
 
 internal class CartBuilder
 {
+    private readonly IProductPriceCalculator productPriceCalculator = new FakeProductPriceCalculator();
     private Func<ShoppingCart> build = () => new ShoppingCart();
+    private Func<ShoppingCart, ShoppingCart>? modify;
 
     public CartBuilder Opened()
     {
@@ -23,11 +28,24 @@ internal class CartBuilder
         return this;
     }
 
+    public CartBuilder WithProduct()
+    {
+        var productId = Guid.NewGuid();
+        const int quantity = 1;
+        modify += cart =>
+        {
+            cart.AddProduct(productPriceCalculator, ProductItem.From(productId, quantity));
+            return cart;
+        };
+        return this;
+    }
+
     public static CartBuilder Create() => new();
 
     public ShoppingCart Build()
     {
         var cart = build();
+        modify?.Invoke(cart);
         ((IAggregate)cart).DequeueUncommittedEvents();
         return cart;
     }
