f1a4ad486e2708efaa58823d9469f19d651e909f
Oskar Dudycz <oskar.dudycz@gmail.com>
2023-03-12 12:09:59 +0100

    Added Scenario to explicitly compose domain logic

---
+++ Sample/ProjectManagement/ProjectManagement/Scenarios/WorkspaceCreation/WorkspaceCreationScenario.cs
@@ -0,0 +1,27 @@
+using Marten;
+using ProjectManagement.Projects;
+using ProjectManagement.Workspaces;
+
+namespace ProjectManagement.Scenarios.WorkspaceCreation;
+
+using static WorkspaceService;
+using static ProjectService;
+
+public static class WorkspaceCreationScenario
+{
+    public static (WorkspaceCreated, ProjectCreated) CreateWorkspace(
+        Func<Guid> generateId,
+        Guid userId,
+        string name,
+        string taskPrefix
+    )
+    {
+        var workspaceId = generateId();
+        var backlogId = generateId();
+
+        return new(
+            Handle(new CreateWorkspace(workspaceId, name, taskPrefix, userId)),
+            Handle(new CreateBacklogProject(backlogId, workspaceId, userId))
+        );
+    }
+}
