85e01c7e699f1ff05b41a57d5aec060bfe5a971f
ronaldkroon <ronald.kroon@wolterskluwer.com>
2023-03-14 10:45:02 +0100

    Changed Concert to Event

--- Sample/Tickets/Tickets/Concerts/Event.cs
+++ Sample/Tickets/Tickets/Concerts/Event.cs
@@ -2,13 +2,13 @@ using Core.Aggregates;
 
 namespace Tickets.Concerts;
 
-public class Concert : Aggregate
+public class Event : Aggregate
 {
     public string Name { get; private set; }
 
     public DateTime Date { get; private set; }
 
-    public Concert(string name, DateTime date)
+    public Event(string name, DateTime date)
     {
         Name = name;
         Date = date;
