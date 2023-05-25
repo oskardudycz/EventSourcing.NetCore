ee1b0efc030441fa7b31a224eb4da71b275dcefb
Oskar Dudycz <oskar.dudycz@gmail.com>
2023-05-25 09:33:46 +0200

    Initial state

--- Sample/Helpdesk/Helpdesk.Api/Core/Marten/DocumentSessionExtensions.cs
+++ Sample/Helpdesk/Helpdesk.Api/Core/Marten/DocumentSessionExtensions.cs
@@ -2,22 +2,3 @@ using Marten;
 
 namespace Helpdesk.Api.Core.Marten;
 
-public static class DocumentSessionExtensions
-{
-    public static Task Add<T>(this IDocumentSession documentSession, Guid id, object @event, CancellationToken ct)
-        where T : class
-    {
-        documentSession.Events.StartStream<T>(id, @event);
-        return documentSession.SaveChangesAsync(token: ct);
-    }
-
-    public static Task GetAndUpdate<T>(
-        this IDocumentSession documentSession,
-        Guid id,
-        int version,
-        Func<T, object> handle,
-        CancellationToken ct
-    ) where T : class =>
-        documentSession.Events.WriteToAggregate<T>(id, version, stream =>
-            stream.AppendOne(handle(stream.Aggregate)), ct);
-}
