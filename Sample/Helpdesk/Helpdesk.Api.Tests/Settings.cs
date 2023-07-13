257c3e5724e858c389092c1f67611d927ef55d1f
Oskar Dudycz <oskar.dudycz@gmail.com>
2023-07-13 14:51:42 +0200

    Meh, had to add this because of the Oakton commands...

--- Sample/Helpdesk/Helpdesk.Api.Tests/Settings.cs
+++ Sample/Helpdesk/Helpdesk.Api.Tests/Settings.cs
@@ -1,3 +1,24 @@
+using Oakton;
 using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
 
 [assembly: CollectionBehavior(DisableTestParallelization = true)]
+
+[assembly: TestFramework("Helpdesk.Api.Tests.AssemblyFixture", "Helpdesk.Api.Tests")]
+
+namespace Helpdesk.Api.Tests;
+
+public sealed class AssemblyFixture : XunitTestFramework, IDisposable
+{
+    public AssemblyFixture(IMessageSink messageSink)
+        :base(messageSink)
+    {
+        OaktonEnvironment.AutoStartHost = true;
+    }
+
+    public new void Dispose()
+    {
+        base.Dispose();
+    }
+}
