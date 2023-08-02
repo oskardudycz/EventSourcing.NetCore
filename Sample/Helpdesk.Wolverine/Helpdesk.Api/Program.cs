1d3dcfea6d05c975e4f0b2dccbbec1a2e0d8dbb0
Oskar Dudycz <oskar.dudycz@gmail.com>
2023-08-02 12:13:38 +0200

    Added Wolverine.Http to move towards endpoints
    Moved incidents endpoints definition to a dedicated file

--- Sample/Helpdesk.Wolverine/Helpdesk.Api/Program.cs
+++ Sample/Helpdesk.Wolverine/Helpdesk.Api/Program.cs
@@ -1,7 +1,5 @@
 using System.Text.Json.Serialization;
-using Helpdesk.Api.Core.Http;
 using Helpdesk.Api.Core.Kafka;
-using Helpdesk.Api.Core.Marten;
 using Helpdesk.Api.Core.SignalR;
 using Helpdesk.Api.Incidents;
 using Helpdesk.Api.Incidents.GetCustomerIncidentsSummary;
@@ -10,22 +8,14 @@ using Helpdesk.Api.Incidents.GetIncidentHistory;
 using Helpdesk.Api.Incidents.GetIncidentShortInfo;
 using JasperFx.CodeGeneration;
 using Marten;
-using Marten.AspNetCore;
 using Marten.Events.Daemon.Resiliency;
 using Marten.Events.Projections;
-using Marten.Pagination;
-using Marten.Schema.Identity;
 using Marten.Services.Json;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 using Oakton;
 using Weasel.Core;
 using Wolverine;
 using Wolverine.Marten;
-using static Microsoft.AspNetCore.Http.TypedResults;
-using static Helpdesk.Api.Incidents.IncidentService;
-using static Helpdesk.Api.Core.Http.ETagExtensions;
-using static System.DateTimeOffset;
 using JsonOptions = Microsoft.AspNetCore.Http.Json.JsonOptions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -89,160 +79,7 @@ builder.Host.UseWolverine();
 
 var app = builder.Build();
 
-var customersIncidents = app.MapGroup("api/customers/{customerId:guid}/incidents/").WithTags("Customer", "Incident");
-var agentIncidents = app.MapGroup("api/agents/{agentId:guid}/incidents/").WithTags("Agent", "Incident");
-var incidents = app.MapGroup("api/incidents").WithTags("Incident");
-
-customersIncidents.MapPost("",
-    async (
-        IDocumentSession documentSession,
-        Guid customerId,
-        LogIncidentRequest body,
-        CancellationToken ct) =>
-    {
-        var (contact, description) = body;
-        var incidentId = CombGuidIdGeneration.NewGuid();
-
-        await documentSession.Add<Incident>(incidentId,
-            Handle(new LogIncident(incidentId, customerId, contact, description, customerId, Now)), ct);
-
-        return Created($"/api/incidents/{incidentId}", incidentId);
-    }
-);
-
-agentIncidents.MapPost("{incidentId:guid}/category",
-    (
-            IDocumentSession documentSession,
-            Guid incidentId,
-            Guid agentId,
-            [FromIfMatchHeader] string eTag,
-            CategoriseIncidentRequest body,
-            CancellationToken ct
-        ) =>
-        documentSession.GetAndUpdate<Incident>(incidentId, ToExpectedVersion(eTag),
-            state => Handle(state, new CategoriseIncident(incidentId, body.Category, agentId, Now)), ct)
-);
-
-agentIncidents.MapPost("{incidentId:guid}/priority",
-    (
-            IDocumentSession documentSession,
-            Guid incidentId,
-            Guid agentId,
-            [FromIfMatchHeader] string eTag,
-            PrioritiseIncidentRequest body,
-            CancellationToken ct
-        ) =>
-        documentSession.GetAndUpdate<Incident>(incidentId, ToExpectedVersion(eTag),
-            state => Handle(state, new PrioritiseIncident(incidentId, body.Priority, agentId, Now)), ct)
-);
-
-agentIncidents.MapPost("{incidentId:guid}/assign",
-    (
-            IDocumentSession documentSession,
-            Guid incidentId,
-            Guid agentId,
-            [FromIfMatchHeader] string eTag,
-            CancellationToken ct
-        ) =>
-        documentSession.GetAndUpdate<Incident>(incidentId, ToExpectedVersion(eTag),
-            state => Handle(state, new AssignAgentToIncident(incidentId, agentId, Now)), ct)
-);
-
-customersIncidents.MapPost("{incidentId:guid}/responses/",
-    (
-            IDocumentSession documentSession,
-            Guid incidentId,
-            Guid customerId,
-            [FromIfMatchHeader] string eTag,
-            RecordCustomerResponseToIncidentRequest body,
-            CancellationToken ct
-        ) =>
-        documentSession.GetAndUpdate<Incident>(incidentId, ToExpectedVersion(eTag),
-            state => Handle(state,
-                new RecordCustomerResponseToIncident(incidentId,
-                    new IncidentResponse.FromCustomer(customerId, body.Content), Now)), ct)
-);
-
-agentIncidents.MapPost("{incidentId:guid}/responses/",
-    (
-        IDocumentSession documentSession,
-        [FromIfMatchHeader] string eTag,
-        Guid incidentId,
-        Guid agentId,
-        RecordAgentResponseToIncidentRequest body,
-        CancellationToken ct
-    ) =>
-    {
-        var (content, visibleToCustomer) = body;
-
-        return documentSession.GetAndUpdate<Incident>(incidentId, ToExpectedVersion(eTag),
-            state => Handle(state,
-                new RecordAgentResponseToIncident(incidentId,
-                    new IncidentResponse.FromAgent(agentId, content, visibleToCustomer), Now)), ct);
-    }
-);
-
-agentIncidents.MapPost("{incidentId:guid}/resolve",
-    (
-            IDocumentSession documentSession,
-            Guid incidentId,
-            Guid agentId,
-            [FromIfMatchHeader] string eTag,
-            ResolveIncidentRequest body,
-            CancellationToken ct
-        ) =>
-        documentSession.GetAndUpdate<Incident>(incidentId, ToExpectedVersion(eTag),
-            state => Handle(state, new ResolveIncident(incidentId, body.Resolution, agentId, Now)), ct)
-);
-
-customersIncidents.MapPost("{incidentId:guid}/acknowledge",
-    (
-            IDocumentSession documentSession,
-            Guid incidentId,
-            Guid customerId,
-            [FromIfMatchHeader] string eTag,
-            CancellationToken ct
-        ) =>
-        documentSession.GetAndUpdate<Incident>(incidentId, ToExpectedVersion(eTag),
-            state => Handle(state, new AcknowledgeResolution(incidentId, customerId, Now)), ct)
-);
-
-agentIncidents.MapPost("{incidentId:guid}/close",
-    async (
-        IDocumentSession documentSession,
-        Guid incidentId,
-        Guid agentId,
-        [FromIfMatchHeader] string eTag,
-        CancellationToken ct) =>
-    {
-        await documentSession.GetAndUpdate<Incident>(incidentId, ToExpectedVersion(eTag),
-            state => Handle(state, new CloseIncident(incidentId, agentId, Now)), ct);
-
-        return Ok();
-    }
-);
-
-customersIncidents.MapGet("",
-    (IQuerySession querySession, Guid customerId, [FromQuery] int? pageNumber, [FromQuery] int? pageSize,
-            CancellationToken ct) =>
-        querySession.Query<IncidentShortInfo>().Where(i => i.CustomerId == customerId)
-            .ToPagedListAsync(pageNumber ?? 1, pageSize ?? 10, ct)
-);
-
-incidents.MapGet("{incidentId:guid}",
-    (HttpContext context, IQuerySession querySession, Guid incidentId) =>
-        querySession.Json.WriteById<IncidentDetails>(incidentId, context)
-);
-
-incidents.MapGet("{incidentId:guid}/history",
-    (HttpContext context, IQuerySession querySession, Guid incidentId) =>
-        querySession.Query<IncidentHistory>().Where(i => i.IncidentId == incidentId).WriteArray(context)
-);
-
-customersIncidents.MapGet("incidents-summary",
-    (HttpContext context, IQuerySession querySession, Guid customerId) =>
-        querySession.Json.WriteById<CustomerIncidentsSummary>(customerId, context)
-);
+app.MapIncidentsEndpoints();
 
 if (app.Environment.IsDevelopment())
 {
@@ -259,32 +96,6 @@ return await app.RunOaktonCommands(args);
 public class IncidentsHub: Hub
 {
 }
-
-public record LogIncidentRequest(
-    Contact Contact,
-    string Description
-);
-
-public record CategoriseIncidentRequest(
-    IncidentCategory Category
-);
-
-public record PrioritiseIncidentRequest(
-    IncidentPriority Priority
-);
-
-public record RecordCustomerResponseToIncidentRequest(
-    string Content
-);
-
-public record RecordAgentResponseToIncidentRequest(
-    string Content,
-    bool VisibleToCustomer
-);
-
-public record ResolveIncidentRequest(
-    ResolutionType Resolution
-);
 
 public partial class Program
 {
