efef1df4a770306a17a5ad0edf938760febbbb66
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-02-06 13:27:02 +0100

    Added idempotent handling of the Resolve incident

--- Sample/Helpdesk.Wolverine/Helpdesk.Api.Tests/Incidents/ResolveIncidentTests.cs
+++ Sample/Helpdesk.Wolverine/Helpdesk.Api.Tests/Incidents/ResolveIncidentTests.cs
@@ -23,6 +23,24 @@ public class ResolveIncidentTests(AppFixture fixture): ApiWithLoggedIncident(fix
 
         await Host.IncidentDetailsShouldBe(Incident with { Status = IncidentStatus.Resolved, Version = 2 });
     }
+    [Fact]
+    [Trait("Category", "Acceptance")]
+    public async Task ResolvingTwiceTheSame_Incident_Succeeds()
+    {
+        var version = Incident.Version;
+        Action<Scenario> resolve = x =>
+        {
+            x.Post.Json(new ResolveIncident(Incident.Id, agentId, resolutionType, version))
+                .ToUrl($"/api/agents/{agentId}/incidents/{Incident.Id}/resolve");
+
+            x.StatusCodeShouldBeOk();
+        };
+        await Host.Scenario(resolve);
+        version++;
+
+        await Host.Scenario(resolve);
+        await Host.IncidentDetailsShouldBe(Incident with { Status = IncidentStatus.Resolved, Version = 2 });
+    }
 
     private readonly Guid agentId = Guid.NewGuid();
     private readonly ResolutionType resolutionType = new Faker().PickRandom<ResolutionType>();
