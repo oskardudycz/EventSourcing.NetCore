5a8fc7afc78df04fbe4b91fe1e241a40999d0295
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-05-02 15:18:06 +0200

    Changed custom IProjections like KafkaProducer, SignalRProducer, ElasticProjection to subscriptions

--- Core.Marten/Subscriptions/MartenEventPublisher.cs
+++ Core.Marten/Subscriptions/MartenEventPublisher.cs
@@ -2,62 +2,71 @@
 using Core.OpenTelemetry;
 using Marten;
 using Marten.Events;
+using Marten.Events.Daemon;
+using Marten.Events.Daemon.Internals;
+using Marten.Events.Projections;
+using Marten.Subscriptions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Core.Marten.Subscriptions;
 
-public class MartenEventPublisher: IMartenEventsConsumer
+public class MartenEventPublisher(
+    IServiceProvider serviceProvider,
+    IActivityScope activityScope,
+    ILogger<MartenEventPublisher> logger
+): SubscriptionBase
 {
-    private readonly IServiceProvider serviceProvider;
-    private readonly IActivityScope activityScope;
-    private readonly ILogger<MartenEventPublisher> logger;
-
-    public MartenEventPublisher(
-        IServiceProvider serviceProvider,
-        IActivityScope activityScope,
-        ILogger<MartenEventPublisher> logger
+    public override async Task<IChangeListener> ProcessEventsAsync(
+        EventRange eventRange,
+        ISubscriptionController subscriptionController,
+        IDocumentOperations operations,
+        CancellationToken token
     )
     {
-        this.serviceProvider = serviceProvider;
-        this.activityScope = activityScope;
-        this.logger = logger;
-    }
-
-    public async Task ConsumeAsync(
-        IDocumentOperations documentOperations,
-        IReadOnlyList<StreamAction> streamActions,
-        CancellationToken cancellationToken
-    )
-    {
-        foreach (var @event in streamActions.SelectMany(streamAction => streamAction.Events))
+        var lastProcessed = eventRange.SequenceFloor;
+        try
         {
-            var parentContext =
-                TelemetryPropagator.Extract(@event.Headers, ExtractTraceContextFromEventMetadata);
+            foreach (var @event in eventRange.Events)
+            {
+                var parentContext =
+                    TelemetryPropagator.Extract(@event.Headers, ExtractTraceContextFromEventMetadata);
 
-            await activityScope.Run($"{nameof(MartenEventPublisher)}/{nameof(ConsumeAsync)}",
-                async (_, ct) =>
-                {
-                    using var scope = serviceProvider.CreateScope();
-                    var eventBus = scope.ServiceProvider.GetRequiredService<IEventBus>();
+                await activityScope.Run($"{nameof(MartenEventPublisher)}/{nameof(ProcessEventsAsync)}",
+                    async (_, ct) =>
+                    {
+                        using var scope = serviceProvider.CreateScope();
+                        var eventBus = scope.ServiceProvider.GetRequiredService<IEventBus>();
 
-                    var eventMetadata = new EventMetadata(
-                        @event.Id.ToString(),
-                        (ulong)@event.Version,
-                        (ulong)@event.Sequence,
-                        parentContext
-                    );
+                        var eventMetadata = new EventMetadata(
+                            @event.Id.ToString(),
+                            (ulong)@event.Version,
+                            (ulong)@event.Sequence,
+                            parentContext
+                        );
 
-                    await eventBus.Publish(EventEnvelope.From(@event.Data, eventMetadata), ct)
-                        .ConfigureAwait(false);
-                },
-                new StartActivityOptions
-                {
-                    Tags = { { TelemetryTags.EventHandling.Event, @event.Data.GetType() } },
-                    Parent = parentContext.ActivityContext
-                },
-                cancellationToken
-            ).ConfigureAwait(false);
+                        await eventBus.Publish(EventEnvelope.From(@event.Data, eventMetadata), ct)
+                            .ConfigureAwait(false);
+
+                        //  TODO:  you can also differentiate based on the exception
+                        // await controller.RecordDeadLetterEventAsync(e, ex);
+                    },
+                    new StartActivityOptions
+                    {
+                        Tags = { { TelemetryTags.EventHandling.Event, @event.Data.GetType() } },
+                        Parent = parentContext.ActivityContext
+                    },
+                    token
+                ).ConfigureAwait(false);
+            }
+
+            return NullChangeListener.Instance;
+        }
+        catch (Exception exc)
+        {
+            logger.LogError("Error while processing Marten Subscription: {ExceptionMessage}", exc.Message);
+            await subscriptionController.ReportCriticalFailureAsync(exc, lastProcessed).ConfigureAwait(false);
+            throw;
         }
     }
 
--- Sample/MartenMeetsElastic/MartenMeetsElastic/Projections/ElasticsearchProjectionConfig.cs
+++ Sample/MartenMeetsElastic/MartenMeetsElastic/Projections/ElasticsearchProjectionConfig.cs
@@ -1,16 +1,17 @@
 using Elastic.Clients.Elasticsearch;
+using Marten;
+using Marten.Events;
 using Marten.Events.Projections;
 
 namespace MartenMeetsElastic.Projections;
 
 public static class ElasticsearchProjectionConfig
 {
-    public static void Add<TElasticsearchProjection>(
-        this ProjectionOptions projectionOptions,
+    public static void AddElasticsearchProjection<TElasticsearchProjection>(
+        this IEventStoreOptions options,
         ElasticsearchClient client
     ) where TElasticsearchProjection : ElasticsearchProjection, new() =>
-        projectionOptions.Add(
-            new TElasticsearchProjection { ElasticsearchClient = client },
-            ProjectionLifecycle.Async
+        options.Subscribe(
+            new TElasticsearchProjection { ElasticsearchClient = client }
         );
 }
