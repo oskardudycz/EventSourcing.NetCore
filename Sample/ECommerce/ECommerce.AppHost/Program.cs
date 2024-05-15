aa70f126f92ef07dc5ff1bab84d347cfe878e6b7
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-05-15 16:17:45 +0200

    Removed redundant API service

--- Sample/ECommerce/ECommerce.AppHost/Program.cs
+++ Sample/ECommerce/ECommerce.AppHost/Program.cs
@@ -1,7 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.ECommerce_ApiService>("apiservice");
-
 var postgres = builder.AddPostgres("postgres-db", password: builder.CreateStablePassword("sqlpassword", minLower: 1, minUpper: 1, minNumeric: 1, minSpecial:0))
     .WithEnvironment("POSTGRES_MULTIPLE_DATABASES", "carts,payments,orders,shipments")
     .WithPgAdmin()
@@ -16,26 +14,6 @@ var cartsDb = postgres
 var ordersDb = postgres.AddDatabase("orders");
 
 var paymentsDB = postgres.AddDatabase("payments");
-//
-// var ordersDB = builder.AddPostgres("orders-db")
-//     .WithEnvironment("POSTGRES_DB", "orders")
-//     .WithPgAdmin()
-//     .WithDataVolume("orders")
-//     .AddDatabase("orders");
-//
-// var paymentsDB = builder.AddPostgres("payments-db")
-//     .WithEnvironment("POSTGRES_DB", "payments")
-//     //.WithHealthCheck()
-//     .WithPgAdmin()
-//     .WithDataVolume("payments")
-//     .AddDatabase("payments");
-
-// var shipmentsDB = builder.AddPostgres("shipments-db")
-//     .WithEnvironment("POSTGRES_DB", "shipments")
-//     .WithHealthCheck()
-//     .WithPgAdmin()
-//     .WithDataVolume()
-//     .AddDatabase("shipments");
 
 var kafka = builder.AddKafka("kafka")
     .WithDataVolume()
@@ -46,14 +24,12 @@ var kafka = builder.AddKafka("kafka")
     .WithEnvironment("TOPIC_CREATE_BACKOFF_MS", "10000");
 
 var shoppingCarts = builder.AddProject<Projects.Carts_Api>("api-shopping-carts")
-    .WithSwaggerUI()
     .WithReference(cartsDb)
     .WithReference(kafka)
     .WaitFor(cartsDb)
     .WaitFor(kafka);
 
 var payments = builder.AddProject<Projects.Payments_Api>("paymentsapi")
-    .WithSwaggerUI()
     //.WithEnvironment("kafka_topics", "['Carts', 'Payments', 'Shipments']")
     .WithReference(paymentsDB)
     .WithReference(kafka)
@@ -61,7 +37,6 @@ var payments = builder.AddProject<Projects.Payments_Api>("paymentsapi")
     .WaitFor(kafka);
 
 var orders = builder.AddProject<Projects.Orders_Api>("api-orders")
-    .WithSwaggerUI()
     //.WithEnvironment("kafka_topics", "['Carts', 'Payments', 'Shipments']")
     .WithReference(ordersDb)
     .WithReference(kafka)
@@ -69,9 +44,5 @@ var orders = builder.AddProject<Projects.Orders_Api>("api-orders")
     .WaitFor(ordersDb)
     .WaitFor(kafka);
 //.WaitForCompletion(payments);
-
-// builder.AddProject<Projects.ECommerce_Web>("webfrontend")
-//     .WithExternalHttpEndpoints()
-//     .WithReference(apiService);
 
 builder.Build().Run();
