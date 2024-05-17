9524267b80e7681f3f96ea43da0b53b470870fa5
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-05-17 13:37:13 +0200

    Added example of Snapshot tests using Verify for catching broken contract in events
    See: https://github.com/VerifyTests/Verify

---
+++ Sample/EventsVersioning/EventsVersioning.Tests/SnapshotTesting/EventsSnapshotTests.cs
@@ -0,0 +1,34 @@
+using System.Runtime.CompilerServices;
+
+namespace EventsVersioning.Tests.SnapshotTesting;
+
+public class EventsSnapshotTests
+{
+    private record ShoppingCartConfirmed(
+        Guid ShoppingCartId,
+        string? ClientId,
+        DateTimeOffset ConfirmedAt
+    );
+
+    [Fact]
+    public Task ShoppingCartConfirmed_WithCompleteData_IsCompatible()
+    {
+        var @event = new ShoppingCartConfirmed(Guid.NewGuid(), "Oskar Dudycz", DateTimeOffset.UtcNow);
+        return Verify(@event);
+    }
+
+    [Fact]
+    public Task ShoppingCartConfirmed_WithOnlyRequiredData_IsCompatible()
+    {
+        var @event = new ShoppingCartConfirmed(Guid.NewGuid(), null, DateTimeOffset.UtcNow);
+        return Verify(@event);
+    }
+}
+// note this is optional, if you really need to
+// This is just showing that you can
+public static class StaticSettingsUsage
+{
+    [ModuleInitializer]
+    public static void Initialize() =>
+        VerifierSettings.AddScrubber(text => text.Replace("Oskar Dudycz", "anonymised"));
+}
