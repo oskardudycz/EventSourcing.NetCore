711ca406a2587958c669dd4cc847a2e37fc409de
Simon Cropp <simon.cropp@gmail.com>
2024-05-19 19:14:41 +1000

    avoid string alloc in SlugGenerator

--- Sample/ProjectManagement/ProjectManagement/Workspaces/Slug/SlugGenerator.cs
+++ Sample/ProjectManagement/ProjectManagement/Workspaces/Slug/SlugGenerator.cs
@@ -3,5 +3,5 @@ namespace ProjectManagement.Workspaces.Slug;
 public static class SlugGenerator
 {
     public static string New(string name) =>
-        $"{name}-{Guid.NewGuid().ToString().Replace("-", "")[..5]}";
+        $"{name}-{Guid.NewGuid().ToString("N")[..5]}";
 }
