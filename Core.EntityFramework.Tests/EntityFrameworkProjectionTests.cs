1af53556f6f96977e6e77c381365a881321ed376
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-05-20 21:25:47 +0200

    Fixed handling for Add, Updates and Removes in the same batch

--- Core.EntityFramework.Tests/EntityFrameworkProjectionTests.cs
+++ Core.EntityFramework.Tests/EntityFrameworkProjectionTests.cs
@@ -69,6 +69,46 @@ public class EntityFrameworkProjectionTests(EfCorePostgresContainerFixture<TestD
         savedEntity.Should().BeNull();
     }
 
+    [Fact]
+    public async Task Applies_Works_In_Batch()
+    {
+        var projection = new ShoppingCartProjection { DbContext = context };
+
+        var cartId = Guid.NewGuid();
+        var clientId = Guid.NewGuid();
+
+        await projection.Handle([
+            EventEnvelope.From(new Opened(cartId, clientId)),
+            EventEnvelope.From(new ProductItemAdded(cartId, 2)),
+            EventEnvelope.From(new ProductItemAdded(cartId, 3)),
+            EventEnvelope.From(new ProductItemAdded(cartId, 5))
+        ], CancellationToken.None);
+        await context.SaveChangesAsync();
+
+        var savedEntity = await context.ShoppingCarts.Where(e => e.Id == cartId).FirstOrDefaultAsync();
+        savedEntity.Should().NotBeNull();
+        savedEntity.Should().BeEquivalentTo(new ShoppingCart { Id = cartId, ClientId = clientId, ProductCount = 10 });
+    }
+
+
+
+    [Fact]
+    public async Task Applies_Works_In_Batch_With_AddAndDeleteOnTheSameRecord()
+    {
+        var projection = new ShoppingCartProjection { DbContext = context };
+
+        var cartId = Guid.NewGuid();
+        var clientId = Guid.NewGuid();
+
+        await projection.Handle([
+            EventEnvelope.From(new Opened(cartId, clientId)),
+            EventEnvelope.From(new Cancelled(cartId))
+        ], CancellationToken.None);
+        await context.SaveChangesAsync();
+
+        var savedEntity = await context.ShoppingCarts.Where(e => e.Id == cartId).FirstOrDefaultAsync();
+        savedEntity.Should().BeNull();
+    }
 
     [Fact]
     public async Task SmokeTest()
