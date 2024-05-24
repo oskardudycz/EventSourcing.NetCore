1b01820fd0e8603d3e9d5e22153a4bb2649d4b3c
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-05-24 09:44:48 +0200

    Configured Nginx as a reverse proxy and load balancer for Docker Compose setup in Helpdesk sample
    Cofigured it fully with Swagger and other Kestrel quirks

--- Sample/Helpdesk/Helpdesk.Api/Incidents/GetCustomerIncidentsSummary/IncidentSummary.cs
+++ Sample/Helpdesk/Helpdesk.Api/Incidents/GetCustomerIncidentsSummary/IncidentSummary.cs
@@ -24,6 +24,7 @@ public class CustomerIncidentsSummaryProjection: MultiStreamProjection<CustomerI
 
     public void Apply(IncidentLogged logged, CustomerIncidentsSummary current)
     {
+        current.Id = logged.CustomerId;
         current.Pending++;
     }
 
