ee4967fd058f0d553f3d7a6f96c6e424db659e70
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-08-29 14:58:53 +0200

    Added the initial Event Driven Architecture Workshop projects structure
    Added also event definition exercises

---
+++ Workshops/EventDrivenArchitecture/Solutions/01-EventsDefinition/EventsDefinitionTests.cs
@@ -0,0 +1,60 @@
+using FluentAssertions;
+using Xunit;
+
+namespace EventsDefinition;
+
+using static GuestStayAccountEvent;
+using static GroupCheckoutEvent;
+
+public class EventsDefinitionTests
+{
+    [Fact]
+    public void GuestStayAccountEventTypes_AreDefined()
+    {
+        // Given
+        var guestStayId = Guid.NewGuid();
+        var groupCheckoutId = Guid.NewGuid();
+
+        // When
+        var events = new GuestStayAccountEvent[]
+        {
+            new GuestCheckedIn(guestStayId, DateTimeOffset.Now),
+            new ChargeRecorded(guestStayId, 100, DateTimeOffset.Now),
+            new PaymentRecorded(guestStayId, 100, DateTimeOffset.Now),
+            new GuestCheckedOut(guestStayId, DateTimeOffset.Now, groupCheckoutId),
+            new GuestStayAccountEvent.GuestCheckoutFailed(guestStayId,
+                GuestStayAccountEvent.GuestCheckoutFailed.FailureReason.NotOpened, DateTimeOffset.Now,
+                groupCheckoutId)
+        };
+
+        // Then
+        const int minimumExpectedEventTypesCount = 5;
+        events.Should().HaveCountGreaterThan(minimumExpectedEventTypesCount);
+        events.GroupBy(e => e.GetType()).Should().HaveCount(minimumExpectedEventTypesCount);
+    }
+
+    [Fact]
+    public void GroupCheckoutEventTypes_AreDefined()
+    {
+        // Given
+        var groupCheckoutId = Guid.NewGuid();
+        Guid[] guestStayIds = [Guid.NewGuid(), Guid.NewGuid()];
+        var clerkId = Guid.NewGuid();
+
+        // When
+        var events = new GroupCheckoutEvent[]
+        {
+            new GroupCheckoutInitiated(groupCheckoutId, clerkId, guestStayIds, DateTimeOffset.Now),
+            new GuestCheckoutsInitiated(groupCheckoutId, guestStayIds, DateTimeOffset.Now),
+            new GuestCheckoutCompleted(groupCheckoutId, guestStayIds[0], DateTimeOffset.Now),
+            new GroupCheckoutEvent.GuestCheckoutFailed(groupCheckoutId, guestStayIds[1], DateTimeOffset.Now),
+            new GroupCheckoutFailed(groupCheckoutId, [guestStayIds[0]], [guestStayIds[1]], DateTimeOffset.Now),
+            new GroupCheckoutCompleted(groupCheckoutId, guestStayIds, DateTimeOffset.Now)
+        };
+
+        // Then
+        const int minimumExpectedEventTypesCount = 3;
+        events.Should().HaveCountGreaterThan(minimumExpectedEventTypesCount);
+        events.GroupBy(e => e.GetType()).Should().HaveCount(minimumExpectedEventTypesCount);
+    }
+}
---
+++ Workshops/EventDrivenArchitecture/Solutions/01-EventsDefinition/GroupCheckoutEvents.cs
@@ -0,0 +1,46 @@
+namespace EventsDefinition;
+
+// 1. TODO: Define your Group Checkout events here
+
+public abstract record GroupCheckoutEvent
+{
+    public record GroupCheckoutInitiated(
+        Guid GroupCheckoutId,
+        Guid ClerkId,
+        Guid[] GuestStayIds,
+        DateTimeOffset InitiatedAt
+    ): GroupCheckoutEvent;
+
+    public record GuestCheckoutsInitiated(
+        Guid GroupCheckoutId,
+        Guid[] InitiatedGuestStayIds,
+        DateTimeOffset InitiatedAt
+    ): GroupCheckoutEvent;
+
+    public record GuestCheckoutCompleted(
+        Guid GroupCheckoutId,
+        Guid GuestStayId,
+        DateTimeOffset CompletedAt
+    ): GroupCheckoutEvent;
+
+    public record GuestCheckoutFailed(
+        Guid GroupCheckoutId,
+        Guid GuestStayId,
+        DateTimeOffset FailedAt
+    ): GroupCheckoutEvent;
+
+    public record GroupCheckoutCompleted(
+        Guid GroupCheckoutId,
+        Guid[] CompletedCheckouts,
+        DateTimeOffset CompletedAt
+    ): GroupCheckoutEvent;
+
+    public record GroupCheckoutFailed(
+        Guid GroupCheckoutId,
+        Guid[] CompletedCheckouts,
+        Guid[] FailedCheckouts,
+        DateTimeOffset FailedAt
+    ): GroupCheckoutEvent;
+
+    private GroupCheckoutEvent() { }
+}
