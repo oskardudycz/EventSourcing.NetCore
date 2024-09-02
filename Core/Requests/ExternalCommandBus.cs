ebb033feb40195e9cc53a491ae41906dd2827204
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-09-02 22:08:15 +0200

    Removed RestSharp dependency, as it's not used anymore

--- Core/Requests/ExternalCommandBus.cs
+++ Core/Requests/ExternalCommandBus.cs
@@ -1,5 +1,4 @@
 using System.Text.Json;
-using RestSharp;
 
 namespace Core.Requests;
 
@@ -15,31 +14,21 @@ public class ExternalCommandBus: IExternalCommandBus
     public Task Post<T>(string url, string path, T command, CancellationToken cancellationToken = default) where T: notnull
     {
         var client = new HttpClient { BaseAddress = new Uri(url) };
-        // //var client = new RestClient(url);
-        //
-        // var request = new RestRequest(path, Method.Post);
-        // request.AddBody(command, ContentType.Json);
 
         return client.PostAsync(path, new StringContent(JsonSerializer.Serialize(command)), cancellationToken);
     }
 
     public Task Put<T>(string url, string path, T command, CancellationToken cancellationToken = default) where T: notnull
     {
-        var client = new RestClient(url);
+        var client = new HttpClient { BaseAddress = new Uri(url) };
 
-        var request = new RestRequest(path, Method.Put);
-        request.AddBody(command, ContentType.Json);
-
-        return client.PutAsync<dynamic>(request, cancellationToken);
+        return client.PutAsync(path, new StringContent(JsonSerializer.Serialize(command)), cancellationToken);
     }
 
     public Task Delete<T>(string url, string path, T command, CancellationToken cancellationToken = default) where T: notnull
     {
-        var client = new RestClient(url);
+        var client = new HttpClient { BaseAddress = new Uri(url) };
 
-        var request = new RestRequest(path, Method.Delete);
-        request.AddBody(command, ContentType.Json);
-
-        return client.DeleteAsync<dynamic>(request, cancellationToken);
+        return client.DeleteAsync(path, cancellationToken);
     }
 }
