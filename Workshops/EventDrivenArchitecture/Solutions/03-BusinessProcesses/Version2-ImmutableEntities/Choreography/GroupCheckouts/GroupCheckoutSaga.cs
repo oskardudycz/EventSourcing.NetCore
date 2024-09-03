f506c6b29e16a5d17212f803684f7f849d0b72db
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-09-03 19:54:44 +0200

    Added stub of choreography

---
+++ Workshops/EventDrivenArchitecture/Solutions/03-BusinessProcesses/Version2-ImmutableEntities/Choreography/GroupCheckouts/GroupCheckoutSaga.cs
@@ -0,0 +1,60 @@
+﻿using BusinessProcesses.Version2_ImmutableEntities.Choreography.GuestStayAccounts;
+
+namespace BusinessProcesses.Version2_ImmutableEntities.Choreography.GroupCheckouts;
+
+using static GroupCheckoutCommand;
+using static GroupCheckoutEvent;
+using static GuestStayAccountCommand;
+using static GuestStayAccountEvent;
+using static SagaResult;
+
+public static class GroupCheckoutSaga
+{
+    public static Command<CheckOutGuest>[] Handle(GroupCheckoutInitiated @event) =>
+        @event.GuestStayIds.Select(guestAccountId =>
+            Send(new CheckOutGuest(guestAccountId, @event.InitiatedAt, @event.GroupCheckoutId))
+        ).ToArray();
+
+    public static SagaResult Handle(GuestCheckedOut @event)
+    {
+        if (!@event.GroupCheckOutId.HasValue)
+            return Ignore;
+
+        return Send(
+            new RecordGuestCheckoutCompletion(
+                @event.GroupCheckOutId.Value,
+                @event.GuestStayId,
+                @event.CheckedOutAt
+            )
+        );
+    }
+
+    public static SagaResult Handle(GuestStayAccountEvent.GuestCheckoutFailed @event)
+    {
+        if (!@event.GroupCheckOutId.HasValue)
+            return Ignore;
+
+        return Send(
+            new RecordGuestCheckoutFailure(
+                @event.GroupCheckOutId.Value,
+                @event.GuestStayId,
+                @event.FailedAt
+            )
+        );
+    }
+};
+
+public abstract record SagaResult
+{
+    public record Command<T>(T Message): SagaResult;
+
+    public record Event<T>(T Message): SagaResult;
+
+    public record None: SagaResult;
+
+    public static Command<T> Send<T>(T command) => new(command);
+
+    public static Event<T> Publish<T>(T @event) => new(@event);
+
+    public static readonly None Ignore = new();
+}
