cb190014fd26f9e5d0dbf27eab936c5eae2e4923
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-09-03 19:55:56 +0200

    Added stub for process managers

---
+++ Workshops/EventDrivenArchitecture/Solutions/03-BusinessProcesses/Version2-ImmutableEntities/ProcessManagers/GuestStayAccounts/GroupCheckoutsConfig.cs
@@ -0,0 +1,18 @@
+﻿using BusinessProcesses.Core;
+using BusinessProcesses.Version2_ImmutableEntities.ProcessManagers.GroupCheckouts;
+using BusinessProcesses.Version2_ImmutableEntities.ProcessManagers.GuestStayAccounts;
+
+namespace BusinessProcesses.Version2_ImmutableEntities.ProcessManagers.GuestStayAccounts;
+
+using static GuestStayAccountCommand;
+
+public static class GuestStayAccountsConfig
+{
+    public static void ConfigureGuestStayAccounts(
+        CommandBus commandBus,
+        GuestStayFacade guestStayFacade
+    )
+    {
+        commandBus.Handle<CheckOutGuest>(guestStayFacade.CheckOutGuest);
+    }
+}
