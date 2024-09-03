c3195a5e1532c7e190ef5e398b8e3e1658c7ee46
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-09-03 19:54:44 +0200

    Added stub of choreography

---
+++ Workshops/EventDrivenArchitecture/Solutions/03-BusinessProcesses/Version2-ImmutableEntities/Choreography/GroupCheckouts/GroupCheckoutsConfig.cs
@@ -0,0 +1,36 @@
+﻿using BusinessProcesses.Core;
+using BusinessProcesses.Version2_ImmutableEntities.Choreography.GuestStayAccounts;
+
+namespace BusinessProcesses.Version2_ImmutableEntities.Choreography.GroupCheckouts;
+
+using static GuestStayAccountEvent;
+using static GroupCheckoutCommand;
+using static SagaResult;
+
+public static class GroupCheckoutsConfig
+{
+    public static void ConfigureGroupCheckouts(
+        EventBus eventBus,
+        CommandBus commandBus,
+        GroupCheckoutFacade groupCheckoutFacade
+    )
+    {
+        eventBus
+            .Subscribe<GroupCheckoutEvent.GroupCheckoutInitiated>((@event, ct) =>
+                commandBus.Send(GroupCheckoutSaga.Handle(@event).Select(c => c.Message).ToArray(), ct)
+            )
+            .Subscribe<GuestCheckedOut>((@event, ct) =>
+                GroupCheckoutSaga.Handle(@event) is Command<RecordGuestCheckoutCompletion>(var command)
+                    ? commandBus.Send([command], ct)
+                    : ValueTask.CompletedTask
+            )
+            .Subscribe<GuestCheckoutFailed>((@event, ct) =>
+                GroupCheckoutSaga.Handle(@event) is Command<RecordGuestCheckoutFailure>(var command)
+                    ? commandBus.Send([command], ct)
+                    : ValueTask.CompletedTask
+            );
+
+        commandBus.Handle<RecordGuestCheckoutCompletion>(groupCheckoutFacade.RecordGuestCheckoutCompletion);
+        commandBus.Handle<RecordGuestCheckoutFailure>(groupCheckoutFacade.RecordGuestCheckoutFailure);
+    }
+}
---
+++ Workshops/EventDrivenArchitecture/Solutions/03-BusinessProcesses/Version2-ImmutableEntities/Choreography/GuestStayAccounts/GroupCheckoutsConfig.cs
@@ -0,0 +1,18 @@
+﻿using BusinessProcesses.Core;
+using BusinessProcesses.Version2_ImmutableEntities.Choreography.GroupCheckouts;
+using BusinessProcesses.Version2_ImmutableEntities.Choreography.GuestStayAccounts;
+
+namespace BusinessProcesses.Version2_ImmutableEntities.Choreography.GuestStayAccounts;
+
+using static GuestStayAccountCommand;
+
+public static class GuestStayAccountsConfig
+{
+    public static void ConfigureGuestStayAccounts(
+        CommandBus commandBus,
+        GuestStayFacade guestStayFacade
+    )
+    {
+        commandBus.Handle<CheckOutGuest>(guestStayFacade.CheckOutGuest);
+    }
+}
