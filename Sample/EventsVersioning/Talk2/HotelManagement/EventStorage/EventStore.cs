31177523e9ebcb7adf40178f3a546828f1e6f332
Oskar Dudycz <oskar.dudycz@gmail.com>
2025-02-02 11:05:43 +0100

    Fix up

---
+++ Sample/EventsVersioning/Talk2/HotelManagement/EventStorage/EventStore.cs
@@ -0,0 +1,52 @@
+﻿namespace HotelManagement.EventStorage;
+
+public interface IEventStore
+{
+    ValueTask AppendToStream(
+        string streamId,
+        IEnumerable<object> newEvents,
+        CancellationToken ct = default
+    );
+
+    ValueTask<object[]> ReadStream(
+        string streamId,
+        CancellationToken ct = default
+    );
+}
+
+public record SerializedEvent(
+    string EventType,
+    string Data,
+    string MetaData = ""
+);
+
+public class InMemoryEventStore(EventSerializer eventSerializer): IEventStore
+{
+    private readonly Dictionary<string, List<SerializedEvent>> events = new();
+
+    public ValueTask AppendToStream(string streamId, IEnumerable<object> newEvents, CancellationToken _ = default)
+    {
+        if (!events.ContainsKey(streamId))
+            events[streamId] = [];
+
+        var serializedEvents = newEvents.Select(eventSerializer.Serialize);
+
+        events[streamId].AddRange(serializedEvents);
+
+        return ValueTask.CompletedTask;
+    }
+
+    public ValueTask<object[]> ReadStream(string streamId, CancellationToken _ = default)
+    {
+        var streamEvents = events.TryGetValue(streamId, out var stream)
+            ? stream
+            : [];
+
+        var deserializedEvents = eventSerializer.Deserialize(streamEvents)
+            .Where(e => e != null)
+            .Cast<object>()
+            .ToArray();
+
+        return ValueTask.FromResult(deserializedEvents);
+    }
+}
