337c29ec816b372b3c6d6c7622d6f811e88ab0df
Oskar Dudycz <oskar.dudycz@gmail.com>
2025-03-03 10:30:26 +0100

    Added core extensions to application logic exercises

---
+++ Workshops/IntroductionToEventSourcing/09-ApplicationLogic.EventStoreDB/Core/Configuration/ConfigurationExtensions.cs
@@ -0,0 +1,14 @@
+namespace Core.Configuration;
+
+public static class ConfigurationExtensions
+{
+    public static T GetRequiredConfig<T>(this IConfiguration configuration, string configurationKey) =>
+        configuration.GetRequiredSection(configurationKey).Get<T>()
+               ?? throw new InvalidOperationException(
+                   $"{typeof(T).Name} configuration wasn't found for '${configurationKey}' key");
+
+    public static string GetRequiredConnectionString(this IConfiguration configuration, string configurationKey) =>
+        configuration.GetConnectionString(configurationKey)
+        ?? throw new InvalidOperationException(
+            $"Configuration string with name '${configurationKey}' was not found");
+}
